f0a41c4de5af0945d819a96ce55cb034dccd066c
Sk1L3D <146962543+termit06@users.noreply.github.com>
2025-01-27 21:02:53 +0300

    Update

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,6 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController
+[RequireComponent(typeof(InputManager))]
+
+public class CarController : MonoBehaviour
 {
-    
+    public InputManager im;
+    public List<WheelCollider> throttleWheels;
+    public List<WheelCollider> steeringWheels;
+    public float strengthCoefficient = 20000f;
+    public float maxTurn = 20f;
+
+    void Start()
+    {
+    im = GetComponent<InputManager>();
+    }
+
+   void FixedUpdate()
+   {
+        foreach(WheelCollider wheel in throttleWheels)
+        {
+            wheel.motorTorque = strengthCoefficient * Time.deltaTime * im.throttle;
+        }
+
+        foreach(WheelCollider wheel in steeringWheels)
+        {
+            wheel.steerAngle = maxTurn * im.steer;
+        }
+   }
 }
