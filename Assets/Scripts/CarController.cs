c288cff0ada2c12b6a79ae6b1017052e4283fc54
termit06 <tatarovs2006@mail.ru>
2025-02-05 15:14:44 +0300

    patch 1

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,32 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(InputManager))]
-
+[RequireComponent(typeof(Rigidbody))]
 public class CarController : MonoBehaviour
 {
     public InputManager im;
     public List<WheelCollider> throttleWheels;
-    public List<WheelCollider> steeringWheels;
-    public float strengthCoefficient = 20000f;
+    public List<GameObject> steeringWheels;
+    public List<GameObject> meshes;
+    public float strengthCoefficient = 10000f;
     public float maxTurn = 20f;
+    public Transform CM;
+    public Rigidbody rb;
 
     void Start()
     {
-    im = GetComponent<InputManager>();
+        im = GetComponent<InputManager>();
+        rb = GetComponent<Rigidbody>();
+        if (CM)
+        {
+            rb.centerOfMass = CM.position;
+        }
     }
 
-   void FixedUpdate()
-   {
-        foreach(WheelCollider wheel in throttleWheels)
+    void FixedUpdate()
+    {
+        foreach (WheelCollider wheel in throttleWheels)
         {
             wheel.motorTorque = strengthCoefficient * Time.deltaTime * im.throttle;
         }
 
-        foreach(WheelCollider wheel in steeringWheels)
+        foreach (GameObject wheel in steeringWheels)
         {
-            wheel.steerAngle = maxTurn * im.steer;
+            wheel.GetComponent<WheelCollider>().steerAngle = maxTurn * im.steer;
+            wheel.transform.localEulerAngles = new Vector3(0f, im.steer * maxTurn, 0f);
         }
-   }
+        foreach (GameObject mesh in meshes)
+        {
+            mesh.transform.Rotate(rb.linearVelocity.magnitude * (transform.InverseTransformDirection(rb.linearVelocity).z >= 0 ? 1 : -1) / (2 * 0.33f), 0f, 0f);
+        }
+    }
 }
